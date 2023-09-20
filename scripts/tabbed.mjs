54459932f35d86772d2892441528239d08b029e0
Pyrofab <pyrofab@ladysnake.org>
2023-09-20 23:24:36 +0200

    Yeet Bootstrap and FA, improve accessibility and responsiveness

--- scripts/tabbed.mjs
+++ scripts/tabbed.mjs
@@ -1,6 +1,12 @@
+function updateAriaSelected(tabbed) {
+    tabbed.querySelectorAll('[role=tab]').forEach((tab) => tab.setAttribute('aria-selected', tab.checked));
+}
+
 function onTabSelected(e) {
     const tabbedKey = e.currentTarget.dataset.tabbedKey;
     const newValue = e.target.value;
+
+    updateAriaSelected(e.currentTarget);
 
     for (const tabbed of document.querySelectorAll(`.tabbed[data-tabbed-key=${tabbedKey}]:not(#${e.currentTarget.id})`)) {
         updateTab(tabbed, newValue);
@@ -25,20 +31,23 @@ function updateTab(tabbed, selectedValue) {
     const tabs = tabbed.querySelectorAll("input[type=radio]");
     let found = false;
     for (const tab of tabs) {
-        if (tab.value === selectedValue) {
-            tab.checked = true;
-            found = true;
-        } else {
-            tab.checked = false;
-        }
+        const c = tab.value === selectedValue;
+        tab.checked = c;
+        tab.setAttribute('aria-selected', c);
+        found |= c;
     }
-    if (!found && tabs.length) tabs[0].checked = true;
+    if (!found && tabs.length) {
+        tabs[0].checked = true;
+        tabs[0].setAttribute('aria-selected', true);
+    }
 }
 
 export function init(tabbed) {
     const persistedTab = localStorage.getItem(`tabbed-${tabbed.dataset.tabbedKey}`);
     if (persistedTab) {
         updateTab(tabbed, persistedTab);
+    } else {
+        updateAriaSelected(tabbed);
     }
 
     tabbed.addEventListener('change', onTabSelected);
