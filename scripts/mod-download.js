6d07e3c9067e8c956bf078c3da49ef5eeafce81d
Pyrofab <pyrofab@ladysnake.org>
2024-01-01 14:56:24 +0100

    Fix mod download dropdown, courtesy of Modrinth's API

--- scripts/mod-download.js
+++ scripts/mod-download.js
@@ -10,71 +10,59 @@
     for use. Yes I did make this comment nice to look at for a reason.
 */
 
+import {getVersions} from "./modrinth-api.js";
+
+
 /**
- * Gets the files for the mod <p>
- * Requires the CurseForge description to contain the version list prefixed with
- * <code>Versions:</code>
- * @param {String} curseID Project ID, probably gotten from the above function
+ *
+ * @param {string} slug
+ * @param {Map<string, {mcVersion: McVersion, modVersions: ModVersion[]}>} versions
+ * @param down
+ * @param {string} downloadIcon
+ * @returns {Promise<void>}
+ */
+async function updateDropdown(slug, versions, down, downloadIcon) {
+    if (versions !== undefined) {
+        const li = document.createElement("li");
+        const warning = document.createElement("em");
+        warning.innerHTML = "WARNING: Not all versions may be supported.";
+        warning.classList.add('download-notice');
+        li.appendChild(warning);
+        down.appendChild(li);
+        for (const [version, versionData] of versions) {
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            let span = document.createElement("span");
+
+            a.href = `https://modrinth.com/mod/${slug}/version/${versionData.modVersions[0].name}`;
+            span.innerHTML += version;
+            span.innerHTML += downloadIcon;
+            span.querySelector("svg").classList.add("download-icon");
+
+            a.appendChild(span);
+            li.appendChild(a);
+            down.appendChild(li);
+        }
+    }
+}
+
+/**
+ * Gets the files for the mod
+ *
+ * @param {String} slug Project slug for use in the Modrinth API
  * @param {String} downloadIcon the HTML code for the download icon
  */
-async function mountModDownloads(curseID, downloadIcon) {
+export async function mountModDownloads(slug, downloadIcon) {
     const down = document.getElementById("mod-download-dropdown");
-    const dataPromise = fetch(`https://curse.nikky.moe/api/addon/${curseID}`).then(it => it.ok && it.json());
-    const descPromise = fetch(`https://curse.nikky.moe/api/addon/${curseID}/description`).then(it => it.ok && it.text());
-    let data;
-    let desc;
     try {
-        data = await dataPromise;
-        desc = await descPromise;
-        if (!data) down.append('Failed to fetch latest downloads');
+        const data = await getVersions(slug);
+        if (data.size === 0) down.append('Failed to fetch latest downloads');
+        else {
+            await updateDropdown(slug, data, down, downloadIcon);
+        }
     } catch (e) {
+        console.error(e);
         down.append('Failed to fetch latest downloads')
     }
-    let flag = false;
 
-    let attempt = setInterval(async function() {
-        if (desc !== undefined && data !== undefined && !flag) {
-            let versions;
-            let flag2;
-            if(/versions:/i.test(desc)) {
-                desc = desc.slice(desc.indexOf("ersions:"), desc.indexOf("ersions:") + 250);
-                desc = desc.replace(/ /g, '-');
-                const reg = /[0-9]\.[0-9]+(\.[0-9]*)*(-Snapshot)?/gi;
-                versions = desc.match(reg);
-                flag2 = true;
-            } else {
-                versions = [...new Set(data.gameVersionLatestFiles.map(f => f.gameVersion))];
-                const li = document.createElement("li");
-                const a = document.createElement("a");
-                a.innerHTML = "WARNING: Not all versions<br />may be supported.";
-                li.appendChild(a);
-                down.appendChild(li);
-            }
-            versions.sort().reverse();
-            for (const version of versions) {
-                let li = document.createElement("li");
-                let a = document.createElement("a");
-                let span = document.createElement("span");
-
-                a.href = `https://curse.nikky.moe/api/url/${curseID}?version=${version}`;
-                span.innerHTML += version;
-                span.innerHTML += downloadIcon;
-                span.querySelector("svg").classList.add("download-icon");
-
-                a.appendChild(span);
-                li.appendChild(a);
-                down.appendChild(li);
-            }
-            if (flag2) {
-                let li = document.createElement("li");
-                let a = document.createElement("a");
-                a.href = data.websiteUrl;
-                a.innerHTML = "More Versions...";
-                li.appendChild(a);
-                down.appendChild(li);
-            }
-            clearInterval(attempt);
-            flag = true;
-        } else if (flag) { clearInterval(attempt); }
-    }, 50);
 }
