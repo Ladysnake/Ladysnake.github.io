deeb876017ee04665e47b925792e6952dc0d64c7
Pyrofab <pyrofab@ladysnake.org>
2024-03-29 23:07:32 +0100

    Redo darkmode toggle as vanilla JS+CSS

--- svelte_sprinkles/src/sprinkles/darkmode/main.ts
+++ svelte_sprinkles/src/sprinkles/darkmode/main.ts
@@ -1,7 +1,9 @@
-import App from './DarkModeToggle.svelte'
-
-const app = new App({
-  target: document.getElementById('darkmode-toggle')!,
-});
-
-export default app
+const darkmodeToggle = document.getElementById('darkmode-toggle');
+if (darkmodeToggle) {
+  darkmodeToggle.addEventListener('click', async () => {
+    // Better avoid loading all of Svelte just for a button, at least wait until someone clicks it
+    const { darkModeEnabled } = await import('../../lib/darkMode');
+    darkModeEnabled.update((v) => !v)
+  });
+  darkmodeToggle.classList.remove('hidden');
+}
