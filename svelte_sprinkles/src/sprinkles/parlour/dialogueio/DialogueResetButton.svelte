7633933b0511a62ccb918bebdb3783b86a4c2c26
Pyrofab <pyrofab@ladysnake.org>
2024-04-07 17:23:01 +0200

    Give the reset confirmation dialog a makeover

--- svelte_sprinkles/src/sprinkles/parlour/dialogueio/DialogueResetButton.svelte
+++ svelte_sprinkles/src/sprinkles/parlour/dialogueio/DialogueResetButton.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
   import {dialogueData} from "../dialogueDataStore";
   import BlabberDialogue from "../model/BlabberDialogue";
+  import ConfirmDialogue from "../../../lib/ConfirmDialogue.svelte";
 
-  function resetDialogue() {
-    if (window.confirm('Do you really want to reset the dialogue?\n\n(You can undo this action by using the "Go Back" button of your browser)')) {
+  let dialog: ConfirmDialogue;
+
+  async function resetDialogue() {
+    if (await dialog.confirm(null)) {
       window.history.pushState({}, '', '#');
       $dialogueData = new BlabberDialogue();
     }
@@ -13,3 +16,8 @@
   <svg class="btn-icon" inline-src="octicon-trashcan-24"/>
   Reset
 </button>
+<ConfirmDialogue bind:this={dialog}>
+  <h4>Do you really want to reset the dialogue?</h4>
+
+  <p class="admonition admonition-important">You can undo this action by using the "Go Back" button of your browser.</p>
+</ConfirmDialogue>
